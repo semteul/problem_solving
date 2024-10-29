dd3c7947ac96ccd61c95f189ebb34205d44f68a0
Kim Minseok <minseok@chungbuk.ac.kr>
2024-10-29 17:29:17 +0900

    [Silver V] Title: 또 수열 문제야, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/31229. 또 수열 문제야/또 수열 문제야.cc
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    long long n;
+    cin >> n;
+
+    for(long long i=1;i<=n;i++) {
+        printf("%lld ",i*2-1);
+    }
+    return 0;
+}
