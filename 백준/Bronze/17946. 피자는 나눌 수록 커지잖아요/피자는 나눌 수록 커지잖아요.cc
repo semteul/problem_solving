b80454c91ceb63a8b34546183a64772a13df4c75
Kim Minseok <minseok@chungbuk.ac.kr>
2024-11-29 01:59:38 +0900

    [Bronze II] Title: 피자는 나눌 수록 커지잖아요, Time: 28 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/17946. 피자는 나눌 수록 커지잖아요/피자는 나눌 수록 커지잖아요.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    for(int i=0;i<n;i++) {
+        int a;
+        cin >> a;
+        cout << 1 << endl;
+    }
+    return 0;
+}
